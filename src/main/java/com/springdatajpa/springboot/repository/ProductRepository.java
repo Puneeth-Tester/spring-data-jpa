e6ed1cbc03d730f9d9136eb37b475499fd7f32c4
Puneeth-Tester <emailtopuneeth@gmail.com>
2024-12-19 19:51:04 +0530

    Spring Data JPA Repository Methods

--- src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
+++ src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
@@ -2,8 +2,6 @@ package com.springdatajpa.springboot.repository;
 
 import com.springdatajpa.springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-// @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
